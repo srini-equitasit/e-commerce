19455b5f5ac09b8ece16b133116fae9fffae6a57
srini-equitasit <srini@equitasitinc.com>
2022-12-15 13:30:54 +0530

    added the jenkin files

---
+++ micro-services/product-price-service/src/main/java/com/equitasit/product_price_service/ProductPriceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.equitasit.product_price_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductPriceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductPriceServiceApplication.class, args);
+	}
+
+}
---
+++ micro-services/product-seller-service/src/main/java/com/equitasit/product_seller_service/ProductSellerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.equitasit.product_seller_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductSellerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductSellerServiceApplication.class, args);
+	}
+
+}
