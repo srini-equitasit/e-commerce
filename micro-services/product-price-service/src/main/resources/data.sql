f938acdfc6a03e91223cd1524c047669a3a2c5a4
srini-equitasit <srini@equitasitinc.com>
2022-12-15 16:42:15 +0530

    configuration updates

---
+++ micro-services/product-price-service/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+--INSERT INTO product (id, name,description) VALUES (10001, 'iPhone','');
+--INSERT INTO product (id, name,description) VALUES (10002, 'Motorola','');
+--INSERT INTO product (id, name,description) VALUES (10003, 'Samsung','');
+--INSERT INTO product (id, name,description) VALUES (10004, 'Nokia','');
