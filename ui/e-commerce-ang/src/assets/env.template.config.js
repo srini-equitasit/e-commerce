0a177e11cc468e9788f785ac777dbbbf1cf6d769
srini-equitasit <srini@equitasitinc.com>
2022-12-31 21:07:12 +0530

    envsubst

--- ui/e-commerce-ang/src/assets/env.template.config.js
+++ ui/e-commerce-ang/src/assets/env.template.config.js
@@ -1,4 +1,8 @@
- // below values will be replaced with envsubst
+// setting environment variables
+// export authDomain=dev-gh3wgihw.jp.auth0.com
+// export clientId=zqLb5a5bYoLPZoU71JAvigGh5N7TmtsZ
+
+// below values will be replaced with envsubst
  // envsubst < env.template.config.js > test.js
 
 sessionStorage.setItem('authDomain','$authDomain');
